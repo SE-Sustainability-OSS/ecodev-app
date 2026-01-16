1690d1d90a7cdb29446cfe39e11bad5843c6fcb9
Dorian Kodelja <dorian.kodelja@se.com>
2026-01-16 16:55:32 +0100

    add demo frontend testing

---
+++ tests_frontend/test_selenium.py
@@ -0,0 +1,96 @@
+import logging
+import os
+import unittest
+from time import sleep
+
+from ecodev_core import engine
+from ecodev_core import SafeTestCase
+from ecodev_core import SETTINGS
+from ecodev_core.app_user import USER_INSERTOR
+from ecodev_core.authentication import _hash_password
+from ecodev_core.db_insertion import create_or_update
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from sqlmodel import Session
+logger = logging.getLogger('selenium')
+logger.setLevel(logging.DEBUG)
+TIMEOUT = 20
+BIG_TIMEOUT = 40
+LOCAL = os.getenv('SELENIUM_LOCAL', 'false').lower() == 'true'
+
+
+class PythonOrgSearchTest(SafeTestCase):
+    @classmethod
+    def setUpClass(cls):
+        """
+        To run this test locally, set environment variable SELENIUM_LOCAL=true
+        """
+        super().setUpClass()
+        print('starting driver')
+        cls.create_test_db()
+        options = webdriver.ChromeOptions()
+        selenium_host = 'localhost' if LOCAL else 'selenium'
+        cls.driver = webdriver.Remote(command_executor=f'http://{selenium_host}:4444/wd/hub',
+                                      options=options)
+
+        print('starting browser')
+        with Session(engine) as session:
+            session.add(create_or_update(session,
+                                         {'id': None,
+                                          'user': SETTINGS.api.user,
+                                          'password': _hash_password(SETTINGS.api.password),
+                                          'permission': 'Consultant',
+                                          'client': 'project_name',
+                                          'application': 'application_name'
+                                          }, USER_INSERTOR))
+        cls.driver.get('http://ecodev_app:80')
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.delete_test_db()
+        cls.driver.quit()
+        return super().tearDownClass()
+
+    def setUp(self):
+        return super().setUp(handle_db=False)
+
+    def tearDown(self):
+        return super().tearDown(handle_db=False)
+
+    def test_project_creation(self):
+        # login
+        print('logintoto')
+        print(SETTINGS.api.user)
+        print(SETTINGS.api.password)
+        login_field = WebDriverWait(self.driver, TIMEOUT).until(
+            EC.element_to_be_clickable((By.XPATH, "//input[@id='{\"index\":\"username\",\"type\":\"login\"}']")))
+        login_field.send_keys(SETTINGS.api.user)
+        password_field = WebDriverWait(self.driver, TIMEOUT).until(
+            EC.element_to_be_clickable((By.XPATH, "//input[@id='{\"index\":\"password\",\"type\":\"login\"}']")))
+        password_field.send_keys(SETTINGS.api.password)
+        sleep(1)
+        self.driver.find_element(
+            By.XPATH, "//button[@id='{\"index\":\"button\",\"type\":\"login\"}']").click()
+
+        new_project_button = WebDriverWait(self.driver, TIMEOUT).until(
+            EC.element_to_be_clickable((By.XPATH, "//button[@id='{\"index\":\"new-project-button-id\",\"type\":\"module-button\"}']")))
+        new_project_button.click()
+
+        print('creating new project')
+        project_name_field = WebDriverWait(self.driver, TIMEOUT).until(
+            EC.element_to_be_clickable((By.XPATH, "//input[@id='{\"index\":\"name\",\"type\":\"project-info-input\"}']")))
+        project_name_field.send_keys('test_project')
+        project_year_field = WebDriverWait(self.driver, TIMEOUT).until(
+            EC.element_to_be_clickable((By.XPATH, "//input[@id='{\"index\":\"year\",\"type\":\"project-info-input\"}']")))
+        project_year_field.send_keys('2022')
+
+        create_project_button = WebDriverWait(self.driver, TIMEOUT).until(
+            EC.element_to_be_clickable((By.XPATH, "//button[@id='{\"index\":\"project-info-save\",\"type\":\"button\"}']")))
+        create_project_button.click()
+
+
+if __name__ == '__main__':
+
+    unittest.main()
